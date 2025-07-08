6e04ac2b8eac8f0675a8e226def0027b467606ab
Anastasia Efremova <anastasia.stoloto@gmail.com>
2025-07-08 10:07:27 +0300

    Some code ti make conflict 4.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,6 +6,6 @@ public class Main {
 
         System.out.println("Hello qaGuru, this is new branch: test_branch");
 
-        System.out.println("Some code to make conflict 3");
+        System.out.println("Some code to make conflict 4");
     }
 }
