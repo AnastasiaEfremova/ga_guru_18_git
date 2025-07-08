a678d7877e3f342b6c0a047e32f30c2d9d56930d
Anastasia Efremova <anastasia.stoloto@gmail.com>
2025-07-08 10:13:42 +0300

    Some code.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,5 +7,6 @@ public class Main {
         System.out.println("Hello qaGuru, this is new branch: test_branch");
 
         System.out.println("Some code to make conflict 6");
+        System.out.println("Some code to make conflict 7");
     }
 }
